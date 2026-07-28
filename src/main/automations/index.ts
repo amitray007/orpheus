94957ba0e95c12d0fef4c3e7e4088cd41f579133
Amit Ray <51674969+amitray007@users.noreply.github.com>
2026-07-28 23:20:54 +0530

    chore(automations): trim result helper export

--- src/main/automations/index.ts
+++ src/main/automations/index.ts
@@ -40,6 +40,5 @@ export function createAutomationRuntime(config: {
 export { AutomationScheduler } from './scheduler'
 export { AutomationService, AutomationDefinitionError } from './service'
 export { createAutomationStore } from './store'
-export { persistableAutomationResult } from './resultPersistence'
 export { AUTOMATION_DEFAULTS, AUTOMATION_LIMITS } from './types'
 export type * from './types'
